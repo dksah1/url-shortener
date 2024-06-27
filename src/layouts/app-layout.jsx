8e7e1ed72d024eb6bd9f5bac6f307051d7c9b1d8
dksah1 <dkboss127126@gmail.com>
2024-06-27 20:30:29 +0545

    issue

--- src/layouts/app-layout.jsx
+++ src/layouts/app-layout.jsx
@@ -1,20 +1,16 @@
 import Header from "@/components/header";
-import React from "react";
 import { Outlet } from "react-router-dom";
 
 const AppLayout = () => {
   return (
-    <div>
-      <main className="min-h-screen container ">
-        {/* header */}
+    <div className="w-full">
+      <main className="min-h-screen container">
         <Header />
-        {/* body */}
         <Outlet />
       </main>
-
-      <div className="p-10 text-center bg-gray-800 mt-10 ">made by dk</div>
-
-      {/* footer */}
+      <div className="p-10 text-center bg-gray-800 mt-10">
+        Made with 💗 by Dk
+      </div>
     </div>
   );
 };
